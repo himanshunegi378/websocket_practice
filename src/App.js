070f578feb7411bd04239ffb1c630206949dfed6
himansunegi378 <davidxavier378@gmail.com>
2020-05-05 06:37:31 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,47 +1,45 @@
-import React, { useState, useEffect,useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 // import './assets/css/theme.min.css'
 import useScript from './components/hooks/useScript';
 import useCss from './components/hooks/loadCss';
 import Dashboard from './components/dashboard/dashboard';
 
-const ws = new WebSocket('ws://localhost:5000/');
+// const ws = new WebSocket('ws://localhost:5000/');
 
 function App() {
-  const [messages, setMessages] = useState([]);
-  const webSocket = useRef(null);
+  const [isPaused, setPause] = useState(false);
+  const [message, setMessage] = useState('')
+  const ws = useRef(null);
 
   useEffect(() => {
-    webSocket.current = new WebSocket("'ws://localhost:5000/");
-    webSocket.current.onmessage = (message) => {
-      setMessages(prev => [...prev, message.data]);
+    ws.current = new WebSocket("ws://localhost:5000/");
+    ws.current.onopen = () => console.log("ws opened");
+    ws.current.onclose = () => console.log("ws closed");
+
+    return () => {
+      ws.current.close();
     };
-    return () => webSocket.current.close();
   }, []);
 
-
-  // useScript(['https://code.jquery.com/jquery-3.4.1.slim.min.js',
-  //   'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js',
-  //   'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js',
-  //   'http://localhost:5000/assets/js/theme.min.js',
-  //   'http://localhost:5000/assets/js/config.navbar-vertical.min.js',
-  // ])
-  useCss('http://localhost:5000/assets/css/theme.min.css')
-
-  // useScript('http://localhost:5000/assets/js/jquery.min.js')
-  // useScript('http://localhost:5000/assets/js/popper.min.js')
-  // useScript('http://localhost:5000/assets/js/bootstrap.min.js')
-  // useScript('http://localhost:5000/assets/js/theme.min.js')
-  useScript(['http://localhost:5000/assets/lib/@fortawesome/all.min.js'])
-  // useScript('http://localhost:5000/assets/lib/stickyfilljs/stickyfill.min.js')
-  // useScript('http://localhost:5000/assets/lib/sticky-kit/sticky-kit.min.js')
-  // useScript('http://localhost:5000/assets/lib/is_js/is.min.js')
-  // useScript('http://localhost:5000/assets/lib/lodash/lodash.min.js')
-  // useScript('http://localhost:5000/assets/lib/perfect-scrollbar/perfect-scrollbar.js')
+  useEffect(() => {
+    if (!ws.current) return;
+console.log('object')
+    ws.current.onmessage = e => {
+      if (isPaused) return;
+      const message = JSON.parse(e.data);
+      // console.log("e", message);
+      setMessage(message)
+    };
+  }, [isPaused]);
 
   return (
-    <div className="App">
-      {messages.join(" ")}
+    <div>
+      <button onClick={() => setPause(!isPaused)}>
+        {isPaused ? "Resume" : "Pause"}
+      </button>
+      {isPaused ? <></> : <p>{message}</p>}
+
     </div>
   );
 }
